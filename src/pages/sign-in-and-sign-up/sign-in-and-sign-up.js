74c4dec38cc8de77130f5aea64b2bdf711fd9750
tauron19 <tauron19@gmail.com>
2022-02-09 03:34:32 -0500

    Sign in and sign up components

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component.js';
+
+import './sign-in-and-sign-up.css';
+
+const SignInAndSignUp = () =>(
+    <div className='sign-in-and-sign-up'>
+        <SignIn/>
+        </div>
+);
+
+export default SignInAndSignUp
