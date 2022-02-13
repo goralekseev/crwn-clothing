740bb03127bcb49e47f55a78836ba6066347791a
tauron19 <tauron19@gmail.com>
2022-02-13 00:14:28 -0500

    sign-in-sign-up page

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component.js';
+import SignUp from '../../components/sign-up/sign-up.component.js';
 
 import './sign-in-and-sign-up.css';
 
 const SignInAndSignUp = () =>(
     <div className='sign-in-and-sign-up'>
         <SignIn/>
+        <SignUp/>
         </div>
 );
 
